c2dda20d5864bff1557e638d8f385050366595b4
Tabuyos <tabuyos@outlook.com>
2020-08-18 17:51:52 +0800

    modified some file

--- OnLisp/onlisp.lisp
+++ OnLisp/onlisp.lisp
@@ -2512,3 +2512,338 @@ x
 
 (defmacro pull (obj place &rest args)
 	`(asetf ,place (delete ,obj it ,@args)))
+
+#|
+	Chapter 17.
+	Read-Macros.
+|#
+
+(set-macro-character #\'
+	#'(lambda (stram char)
+		  (declare (ignore char))
+		  (list 'quote (read stream t nil t))))
+
+;; ' is oldest read-macro in lisp, you can 'a -> (quote a), but this very bother and reduce code readability
+
+(setq a 1)
+
+(defmacro q (obj)
+	`(quote ,obj))
+
+(eq 'c (q c))
+
+(q (q a)) ;; error
+
+(mac (q (q a))) ;; '(Q A)
+
+(set-dispatch-macro-character #\# #\?
+	#'(lambda (stream char1 char2)
+		  (declare (ignore char1 char2))
+		  `#'(lambda (&rest ,(gensym))
+				 ,(read stream t nil t))))
+
+(mapcar #?2 '(a b c))
+
+(eq (funcall #?'a) 'a)
+
+(eq (funcall #?#'oddp) (symbol-function 'oddp))
+
+(set-macro-character #\] (get-macro-character #\)))
+
+(set-dispatch-macro-character #\# #\[
+	#'(lambda (stream char1 char2)
+		  (declare (ignore char1 char2))
+		  (let ((accum nil)
+				   (pair (read-delimited-list #\] stream t)))
+			  (do ((i (ceiling (car pair)) (1+ i)))
+				  ((> i (floor (cadr pair)))
+				   (list 'quote (nreverse accum)))
+				  (push i accum)))))
+
+(format t "~A" #[2 7])
+
+(defmacro defdelim (left right parms &body body)
+	`(ddfn ,left ,right #'(lambda ,parms ,@body)))
+
+(let ((rpar (get-macro-character #\))))
+	(defun ddfn (left right fn)
+		(set-macro-character right rpar)
+		(set-dispatch-macro-character #\# left
+			#'(lambda (stream char1 char2)
+				  (declare (ignore char1 char2))
+				  (apply fn (read-delimited-list right stream t))))))
+
+(defdelim #\{ #\} (&rest args)
+	`(fn (compose ,@args)))
+
+(defmacro quotable ()
+	'(list 'able))
+
+(defmacro quotable ()
+	(quote (list (quote able))))
+
+#|
+	Chapter 18.
+	Destructuring.
+|#
+
+;; CLTL1
+(let ((x (first lst))
+		 (y (second lst))
+		 (z (third lst)))
+	...)
+
+;; CLTL2
+(destructuring-bind (x y z) lst
+	...)
+
+;; destructuring-bind's first argument is any tree.
+;; ex.
+(destructuring-bind ((first last) (month day year) . notes) birthday
+	...)
+
+(defmacro dbind (pat seq &body body)
+	(let ((gseq (gensym)))
+		`(let ((,gseq ,seq))
+			 ,(dbind-ex (destruc pat gseq #'atom) body))))
+
+(defun destruc (pat seq &optional (atom? #'atom) (n 0))
+	(if (null pat)
+		nil
+		(let ((rest (cond ((funcall atom? pat) pat)
+						((eq (car pat) '&rest) (cadr pat))
+						((eq (car pat) '&body) (cadr pat))
+						(t nil))))
+			(if rest
+				`((,rest (subseq ,seq ,n)))
+				(let ((p (car pat))
+						 (rec (destruc (cdr pat) seq atom? (1+ n))))
+					(if (funcall atom? p)
+						(cons `(,p (elt ,seq ,n))
+							rec)
+						(let ((var (gensym)))
+							(cons (cons `(,var (elt ,seq ,n))
+									  (destruc p var atom?))
+								rec))))))))
+
+(defun dbind-ex (binds body)
+	(if (null binds)
+		`(progn ,@body)
+		`(let ,(mapcar #'(lambda (b)
+							 (if (consp (car b))
+								 (car b)
+								 b))
+				   binds)
+			 ,(dbind-ex (mapcan #'(lambda (b)
+									  (if (consp (car b))
+										  (cdr b)))
+							binds)
+				  body))))
+
+(dbind (a b c) #(1 2 3) (list a b c))
+
+(dbind (a (b c) d) '(1 #(2 3) 4) (list a b c d))
+
+(dbind (a (b . c) &rest d) '(1 "fribble" 2 3 4) (list a b c d))
+;; #(: vector #\: char "abc" = #(#\a #\b #\c)
+
+(destruc '(a b c) 'seq #'atom)
+
+(destruc '(a (b . c) &rest d) 'seq)
+
+(dbind-ex (destruc '(a (b . c) &rest d) 'seq) '(body))
+
+(dbind (a b c) (list 1 2))
+
+(setq ar (make-array '( 3 3)))
+
+(for (r 0 2)
+	(for (c 0 2)
+		(setf (aref ar r c) (+ (* r 10) c))))
+
+(with-matrix ((a b c)
+				 (d e f)
+				 (g h i)) ar
+	(list a b c d e f g h i))
+
+(defmacro with-matrix (pats ar &body body)
+	(let ((gar (gensym)))
+		`(let ((,gar ,ar))
+			 (let ,(let ((row -1))
+					   (mapcan
+						   #'(lambda (par)
+								 (incf row)
+								 (let ((col -1))
+									 (mapcar #'(lambda (p)
+												   `(,p (aref ,gar ,row ,(incf col))))
+										 par)))
+						   pats))
+				 ,@body))))
+
+(defmacro with-array (pat ar &body body)
+	(let ((gar (gensym)))
+		`(let ((,gar ,ar))
+			 (let ,(mapcar #'(lambda (p)
+								 `(,(car p) (aref ,gar ,@(cdr p))))
+					   pat)
+				 ,@body))))
+
+(with-array ((a 0 0) (d 1 1) (i 2 2)) ar (values a d i))
+
+(defmacro with-struct ((name . fields) struct &body body)
+	(let ((gs (gensym)))
+		`(let ((,gs ,struct))
+			 (let ,(mapcar #'(lambda (f)
+								 `(,f (,(symb name f) ,gs)))
+					   fields)
+				 ,@body))))
+
+(defstruct visitor name title firm)
+
+(setq theo (make-visitor :name "Theodebert" :title 'king :firm 'franks))
+
+(with-struct (visitor- name firm title) theo
+	(list name firm title))
+
+(with-slots (species age height) my-tree
+	...)
+
+(setq height 72)
+
+(defmacro with-places (pat seq &body body)
+	(let ((gseq (gensym)))
+		`(let ((,gseq ,seq))
+			 ,(wplac-ex (destruc pat gseq #'atom) body))))
+
+(defun wplac-ex (binds body)
+	(if (null binds)
+		`(progn ,@body)
+		`(symbol-macrolet ,(mapcar #'(lambda (b)
+										 (if (consp (car b))
+											 (car b)
+											 b))
+							   binds)
+			 ,(wplac-ex (mapcan #'(lambda (b)
+									  (if (consp (car b))
+										  (cdr b)))
+							binds)
+				  body))))
+
+(with-places (a b c) #(1 2 3)
+	(list a b c))
+
+;; must be use setf!
+(let ((lst '(1 (2 3) 4)))
+	(with-places (a (b . c) d) lst
+		(setf a 'uno)
+		(setf c '(tre)))
+	lst)
+
+;; dbind faster than with-places
+
+(match '(p a b c a) '(p ?x ?y c ?x))
+
+(match '(p ?x b ?y a) '(p /y b c a))
+
+(match '(a b c) '(a a a))
+
+(match '(p ?x) '(p ?x))
+
+(defun match (x y &optional binds)
+	(acond2
+		((or (eql x y) (eql x '_) (eql y '_)) (values binds t))
+		((binding x binds) (match it y binds))
+		((binding y binds) (match x it binds))
+		((varsym? x) (values (cons (cons x y) binds) t))
+		((varsym? y) (values (cons (cons y x) binds) t))
+		((and (consp x) (consp y) (match (car x) (car y) binds))
+			(match (cdr x) (cdr y) it))
+		(t (values nil nil))))
+
+(defun varsym? (x)
+	(and (symbolp x) (eq (char (symbol-name x) 0) #\?)))
+
+(defun binding (x binds)
+	(labels ((recbind (x binds)
+				 (aif (assoc x binds)
+					 (or (recbind (cdr it) binds)
+						 it))))
+		(let ((b (recbind x binds)))
+			(values (cdr b) b))))
+
+(match '(a ?x b) '(_ 1 _))
+
+(defmacro if-match (pat seq then &optional else)
+	`(aif2 (match ',pat ,seq)
+		 (let ,(mapcar #'(lambda (v)
+							 `(,v (binding ',v it)))
+				   (vars-in then #'atom))
+			 ,then)
+		 ,else))
+
+(defun vars-in (expr &optional (atom? #'atom))
+	(if (funcall atom? expr)
+		(if (var? expr) (list expr))
+		(union (vars-in (car expr) atom?)
+			(vars-in (cdr expr) atom?))))
+
+(defun var? (x)
+	(and (symbolp x) (eq (char (symbol-name x) 0) #\?)))
+
+(defun abab (seq) (if-match (?x ?y /x ?y) seq (values ?x ?y) nil))
+
+(abab '(hi ho hi ho))
+
+(defmacro if-match (pat seq then &optional else)
+	`(let ,(mapcar #'(lambda (v) `(,v ',(gensym)))
+			   (vars-in pat #'simple?))
+		 (pat-match ,pat ,seq ,then ,else)))
+
+(defmacro pat-match (pat seq then else)
+	(if (simple? pat)
+		(match1 `((,pat ,seq)) then else)
+		(with-gensyms (gseq gelse)
+			`(labels ((,gelse () ,else))
+				 ,(gen-match (cons (list gseq seq)
+								 (destruc pat gseq #'simple?))
+					  then
+					  `(,gelse))))))
+
+(defun simple? (x) (or (atom x) (eq (car x) 'quote)))
+
+(defun gen-match (refs then else)
+	(if (null refs)
+		then
+		(let ((then (gen-match (cdr refs) then else)))
+			(if (simple? (caar refs))
+				(match1 refs then else)
+				(gen-match (car refs) then else)))))
+
+(defun match1 (refs then else)
+	(dbind ((pat expr) . rest) regs
+		(cond ((gensym? pat)
+				  `(let ((,pat ,expr))
+					   (if (and (typep ,pat 'sequence)
+							   ,(length-test pat rest))
+						   ,then
+						   ,else)))
+			((eq pat '_) then)
+			((var? pat)
+				(let ((ge (gensym)))
+					`(let ((,ge ,expr))
+						 (if (or (gensym? ,pat) (equal ,pat ,ge))
+							 (let ((,pat ,ge)) ,then)
+							 ,else))))
+			(t `(if (equal ,pat ,expr) ,then ,else)))))
+
+(defun gensym? (s)
+	(and (symbolp s) (not (symbol package s))))
+
+(defun length-test (pat rest)
+	(let ((fin (caadar (last rest))))
+		(if (or (consp fin) (eq fin 'elt))
+			`(= (length ,pat) ,(length rest))
+			`(> (length ,pat) ,(- (length rest) 2)))))
+
+(if-match (?x 'a) seq (print ?x) nil)
+
